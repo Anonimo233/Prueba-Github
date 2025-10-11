af9c4d4d98b936d9e024c0d220d1f53f69cf5057
Not3mmx <76117838+Anonimo233@users.noreply.github.com>
2025-10-11 18:12:22 -0500

    UPCPRUEBA2.py

--- UPCPRUEBA2.py
+++ UPCPRUEBA2.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import random
-import plotly.express as px
 
 # Configuración general
 st.set_page_config(page_title="Tecnología en Lima Metropolitana", page_icon="💻", layout="centered")
